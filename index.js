77aaea7bc5c36404ea14c482f37027b23d0078b7
minhtrannguyen-git <81168753+minhtrannguyen-git@users.noreply.github.com>
2022-07-02 14:33:15 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res)=>{
+    return res.send("Hello World!");
+})
+
+app.listen(port, ()=>{console.log(`The website is at http://localhost:${port}`)})
